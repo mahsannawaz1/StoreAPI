348d2d77db56cab33dc4d2cb579bf9a54834030a
Ahsan Nawaz <nawazehsen@gmail.com>
2023-07-06 18:56:02 +0500

    Added Customer and its related models

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Product,ProductImage
+from .models import Product,ProductImage,Customer,Address,Review,Comment
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(ProductImage)
+admin.site.register(Customer)
+admin.site.register(Address)
+admin.site.register(Review)
+admin.site.register(Comment)
--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.conf import settings
 # Create your models here.
 
 class Product(models.Model):
@@ -12,8 +13,55 @@ class Product(models.Model):
   def __str__(self):
     return f'{self.title}'
 
+
 class ProductImage(models.Model):
   image=models.ImageField(upload_to='products',default='default.png')
   product=models.ForeignKey(Product,on_delete=models.CASCADE,related_name='images')
   def __str__(self):
     return f'{self.product.title} image'
+
+
+class Customer(models.Model):
+  MEMBERSHIP_BRONZE='B'
+  MEMBERSHIP_SILVER='S'
+  MEMBERSHIP_GOLD='G'
+  MEMBERSHIP_CHOICES=[
+    (MEMBERSHIP_GOLD,'Gold'),
+    (MEMBERSHIP_SILVER,'Silver'),
+    (MEMBERSHIP_BRONZE,'Bronze'),
+  ]
+  phone=models.CharField(max_length=15)
+  birth_date=models.DateField(null=True,blank=True)
+  user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='customer')
+  membership=models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+
+  def __str__(self):
+    return f'{self.user.first_name} {self.user.last_name} ({self.user.username})'
+
+class Address(models.Model):
+  street = models.TextField()
+  city=models.CharField(max_length=20)
+  state=models.CharField(max_length=20)
+  country=models.CharField(max_length=20)
+  customer=models.OneToOneField(Customer,on_delete=models.CASCADE,related_name='address')
+
+  def __str__(self):
+    return f"{self.customer.user.username}'s Address"
+class Comment(models.Model):
+  description=models.TextField()
+  customer=models.ForeignKey(Customer, on_delete=models.CASCADE,related_name='comments',null=True,blank=True)
+  product=models.ForeignKey(Product, on_delete=models.CASCADE,related_name='comments')
+  posted_on=models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"{self.customer.user.username}'s Comment {self.id}"
+
+class Review(models.Model):
+  title=models.CharField(max_length=30)
+  description=models.TextField()
+  rating=models.PositiveSmallIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+  customer=models.ForeignKey(Customer, on_delete=models.CASCADE,related_name='reviews',null=True,blank=True)
+  product=models.ForeignKey(Product, on_delete=models.CASCADE,related_name='reviews')
+  posted_on=models.DateTimeField(auto_now_add=True)
+  def __str__(self):
+    return f"{self.customer.user.username}'s Review {self.id}"
