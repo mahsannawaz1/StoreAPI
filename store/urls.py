093f8594704011d20580253bd61af8fb479e0ae6
Ahsan Nawaz <nawazehsen@gmail.com>
2023-07-06 16:18:42 +0500

    Added CRUD functionaity for the products model

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('api/products/',views.product_list,name='product-list'),
-    path('api/products/<int:pk>/',views.product_detail,name='product-detail'),
+    path('api/products/',views.ListCreateProductAPIView.as_view()),  
+    path('api/products/<int:pk>/',views.RetrieveUpdateDeleteProductAPIView.as_view()),
 ] 
--- store/views.py
+++ store/views.py
@@ -1,24 +1,62 @@
 from django.shortcuts import render,get_object_or_404
+from rest_framework.views import APIView
 
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import api_view,permission_classes
 from .models import Product
 from django.core.exceptions import ObjectDoesNotExist
 from .serializers import ProductSerializer
 # Create your views here.
 
-@api_view()
-def product_list(request):
-  products=Product.objects.prefetch_related('images').all()
-  serializer=ProductSerializer(products,many=True)
-  return Response(serializer.data,status=status.HTTP_200_OK)
 
+class ListCreateProductAPIView(APIView):
 
-@api_view()
-def product_detail(request,pk):
+  http_method_names=['get', 'post']
+
+  def get_permissions(self):
+    if self.request.method =='POST':
+      return [IsAdminUser()]
+    return super().get_permissions()
+
+  def get(self,request):
+    products=Product.objects.prefetch_related('images').all()
+    serializer=ProductSerializer(products,many=True)
+    return Response(serializer.data,status=status.HTTP_200_OK)
+
+  def post(self,request):
+    serializer=ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+class RetrieveUpdateDeleteProductAPIView(APIView):
+
+  http_method_names=['get', 'put','delete']
+
+  def get_permissions(self):   
+    if self.request.method in ['PUT','DELETE']:
+      return [IsAdminUser()]
+    return super().get_permissions()
+
+  def get(self,request,pk):
+    product=get_object_or_404(Product,id=pk)
+    serializer=ProductSerializer(product,many=False)
+    return Response(serializer.data,status=status.HTTP_200_OK)
+
+  def put(self,request,pk):
+    product=get_object_or_404(Product,id=pk)
+    serializer=ProductSerializer(product,data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
   
-  product=get_object_or_404(Product,id=pk)
-  serializer=ProductSerializer(product,many=False)
-  return Response(serializer.data,status=status.HTTP_200_OK)
+  def delete(self,request,pk):
+    product=get_object_or_404(Product,id=pk)
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
  
+
+
+
